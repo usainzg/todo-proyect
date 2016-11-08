4c804ceefca4bd4ad6d115b4ffea96540c49e88c
Unai Sainz de la Maza Gamboa <unaihtc70@gmail.com>
2016-11-08 17:04:47 +0100

    Fragments for tasks list added.

--- Proyect/app/src/main/java/com/codi6/proyect/activity/MainActivity1.java
+++ Proyect/app/src/main/java/com/codi6/proyect/activity/MainActivity1.java
@@ -2,22 +2,21 @@ package com.codi6.proyect.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.NavigationView.OnNavigationItemSelectedListener;
 import android.support.design.widget.Snackbar;
-import android.support.v4.view.MenuCompat;
-import android.support.v4.view.MenuItemCompat;
-import android.support.v7.widget.SearchView;
-import android.support.v7.widget.SearchView.OnQueryTextListener;
-import android.util.Log;
-import android.view.View;
-import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.SearchView.OnQueryTextListener;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.codi6.proyect.R;
 
